4b37556ea5acc217176fac9b79396a0a0b43c9d7
lordvitor11 <65216891+lordvitor11@users.noreply.github.com>
2020-10-12 00:18:42 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,14 +4,17 @@ function carregar() {
     var data = new Date()
     var hora  = data.getHours() 
     msg.innerHTML = `Agora são ${hora} horas.`
-    if (hora >= 0 && hora < 12) {
+    if (hora >= 6 && hora < 12) {
         img.src = "fotomanha.png"
         document.body.style.background = "#a2c4de"
     }else if (hora >= 12 && hora < 18) {
         img.src = "fototarde.png"
         document.body.style.background = "#f46077"
-    }else {
+    }else if (hora >= 18 && hora < 00) {
         img.src = "fotonoite.png"
         document.body.style.background = "#035963"
+    }else {
+        img.src = "fotomadruga.png"
+        document.body.style.background = "#353839"
     }
 }
